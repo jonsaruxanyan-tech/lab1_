c40505e979d4a255d9f3294b13a0c14666f9d2d3
jonsaruxanyan-tech <jonsaruxanyan@gmail.com>
2026-02-10 09:53:25 +0300

    Update new.cpp

--- new/new.cpp
+++ new/new.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+//some new messages
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Anahit, hello!" << std::endl;
+    std::cout << "Anahit!" << std::endl;
     return 0;
+    
 }
